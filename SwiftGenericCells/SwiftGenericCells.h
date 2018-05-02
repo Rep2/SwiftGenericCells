1259546f7b0b024ba4935ba735dd199b52f3eb45
Ivan Rep <ivan.rep2@gmail.com>
2018-05-02 13:19:35 +0200

    Initial commit

---
+++ SwiftGenericCells/SwiftGenericCells.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGenericCells.h
+//  SwiftGenericCells
+//
+//  Created by Ivan Rep on 02/05/2018.
+//  Copyright © 2018 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGenericCells.
+FOUNDATION_EXPORT double SwiftGenericCellsVersionNumber;
+
+//! Project version string for SwiftGenericCells.
+FOUNDATION_EXPORT const unsigned char SwiftGenericCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGenericCells/PublicHeader.h>
+
+
