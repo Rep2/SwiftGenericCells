02aa1668c2240e0beec6d92142ae450b674b51f7
Ivan Rep <ivan.rep2@gmail.com>
2018-05-02 21:57:39 +0200

    TitleTableViewCell

--- SwiftGenericCells/SimpleTableViewCells/TitleTableViewCell.swift
+++ SwiftGenericCells/SimpleTableViewCells/TitleTableViewCell.swift
@@ -1,3 +1,32 @@
+import SwiftViewModels
+import SnapKit
 import UIKit
 
-public class TitleTableViewCell: UITableViewCell {}
+public struct TitleTableViewCellViewModel {
+    public let titleViewModel: TextViewModel
+
+    public init(titleViewModel: TextViewModel) {
+        self.titleViewModel = titleViewModel
+    }
+}
+
+public class TitleTableViewCell: UITableViewCell {
+    lazy var titleLabel: UILabel = {
+        return UILabel()
+    }()
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.addSubview(titleLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.left.right.equalTo(contentView).inset(UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+            make.centerY.equalTo(contentView)
+        }
+    }
+
+    public func present(viewModel: TitleTableViewCellViewModel) {
+        viewModel.titleViewModel.apply(toLabel: titleLabel)
+    }
+}
